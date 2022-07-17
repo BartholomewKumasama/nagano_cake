597e9f4914e3271103ca03ea6fa63056d0fd3a11
EC2 Default User <ec2-user@ip-172-31-17-208.ap-northeast-1.compute.internal>
2022-07-17 06:20:14 +0000

    error

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_07_17_023746) do
     t.integer "total_payment"
     t.integer "payment_method", default: 0
     t.integer "status", default: 0
+  
 
 
   create_table "cart_items", force: :cascade do |t|
