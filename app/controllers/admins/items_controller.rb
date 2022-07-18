08a1bee63867a03c64bde12ce52ac3642af6771d
EC2 Default User <ec2-user@ip-172-31-35-105.ap-northeast-1.compute.internal>
2022-07-18 13:08:48 +0000

    admins/items_index.create.new.show作成途中でのpush

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,19 +1,44 @@
 class Admins::ItemsController < ApplicationController
+  
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admins_item_path(@item)
+      flash[:notice] = '新しい商品を登録しました。'
+    else
+      render "new"
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admins_item_path(@item.id)
+    else
+      render "edit"
+      flash[:notice] = '新しい商品を登録しました。'
+    end
   end
 end
+
+private
+
+  def item_params
+  	params.require(:item).permit(:genre_id,:name,:explanation,:price,:is_stock,:item_image)
+  end
