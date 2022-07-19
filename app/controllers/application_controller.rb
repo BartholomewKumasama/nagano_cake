39211161e7583110ffc81ee20ca0fb86139816a3
EC2 Default User <ec2-user@ip-172-31-17-208.ap-northeast-1.compute.internal>
2022-07-19 10:54:42 +0000

    とりあ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_admin!, if: :admin_url # admin_urlを参照する
+  before_action :configure_permitted_parameters , if: :devise_controller?
 
-  def admin_url
-    request.fullpath.include?("/admin") #pathにadminが含まれているページは、adminでログインしないと見れない設定
-  end
-
+ def after_sign_in_path_for(resource_or_scope)
+     if resource.is_a?(Admin)
+        admin_orders_path
+      else
+        root_path
+      end
+    end
 end
