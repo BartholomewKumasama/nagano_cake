f93a153bc4d57e54c53cd5b6e0f25fc7aa955314
taisei_ueki <dachengzhimu044@gmail.com>
2022-07-19 08:57:22 +0000

    add customer column

---
+++ db/migrate/20220719052735_add_columns_to_customers.rb
@@ -0,0 +1,14 @@
+class AddColumnsToCustomers < ActiveRecord::Migration[6.1]
+  def change
+
+    add_column :customers, :family_name,      :string , null: false                 ##名
+    add_column :customers, :first_name,       :string , null: false                 ##姓
+    add_column :customers, :family_name_kana, :string , null: false                 ##名カナ
+    add_column :customers, :first_name_kana,  :string , null: false                 ##姓カナ
+    add_column :customers, :postal_code,      :string , null: false                 ##郵便番号
+    add_column :customers, :address,          :string , null: false                 ##住所
+    add_column :customers, :telephone_number, :string , null: false                 ##電話番号
+    add_column :customers, :is_deleted,       :boolean, null: false, default: false ##退会フラグ
+    
+  end
+end
