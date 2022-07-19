12985f413495fe40ae3bcf167c721ecbbf3b7632
EC2 Default User <ec2-user@ip-172-31-35-105.ap-northeast-1.compute.internal>
2022-07-19 13:03:22 +0000

    Revert "routes.rb修正"
    This reverts commit 257c5c04339d139a482344a4902944635a6ca085.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
 
+    # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+<<<<<<< HEAD
+<<<<<<< HEAD
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+=======
+=======
+>>>>>>> parent of 72f65aa (Revert "devse/admin.customer修正")
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -14,7 +31,11 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 
 
-root to: 'publics/homes#top'
+<<<<<<< HEAD
+>>>>>>> parent of 72f65aa (Revert "devse/admin.customer修正")
+=======
+>>>>>>> parent of 72f65aa (Revert "devse/admin.customer修正")
+  root to: 'publics/homes#top'
   get 'publics/homes/about' => 'publics/homes#about', as: 'about'
 
 
