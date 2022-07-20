cd3e03988c881a34e7921b8ef473dbed8f65c6e1
EC2 Default User <ec2-user@ip-172-31-17-208.ap-northeast-1.compute.internal>
2022-07-20 13:10:05 +0000

    _headerの大部分は完成、修正は後々必要になる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters , if: :devise_controller?
 
- def after_sign_in_path_for(resource_or_scope)　#複数のdevice
+ def after_sign_in_path_for(resource_or_scope)　#複数のdeviceがある場合この記述をする
     if resource.is_a?(Admin)
        admin_orders_path
     else
