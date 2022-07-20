4b5cf750dbd0465d8845d0d680d7f268f4f2f043
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-07-20 05:23:05 +0000

    “エラー解消”

--- config/routes.rb
+++ config/routes.rb
@@ -22,19 +22,19 @@ Rails.application.routes.draw do
     resources :orders, only:[:new,:index,:show,:create]
     post 'orders/confirm'
     post 'orders/complete'
-<<<<<<< HEAD
+
     
     resources :cart_items, only:[:index,:create,:update,:destroy] 
     delete 'cart_items/destroy_all' 
     
     resources :cart_items, only:[:show,:edit,:update] 
-=======
+
 
     resources :cart_items, only:[:index,:create,:update,:destroy]
     delete 'cart_items/destroy_all'
 
     resources :cart_items, only:[:show,:edit,:update]
->>>>>>> origin/develop
+
     get 'customers/unsubscribe'
     patch 'customers/withdrawal'
     
