892761abe1470fa77132ba7d6b0bd6096d751250
EC2 Default User <ec2-user@ip-172-31-17-208.ap-northeast-1.compute.internal>
2022-07-21 10:32:51 +0000

    一旦

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,19 +1,40 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total = @order.total_price-800 #送料
+  end
+  
+  def create
+     @order = Order.new(order_params)
+      cart_item = current_public.cart_items.all
+     @order.save
+      current_public.cart_items.each do | cart_item |
+      order_detail = OrderDetail.new
+      order_detail.order_id = @order.id
+      order_detail.item_id = cart_item.item_id
+      order_detail.price = cart_item.item.price
+      order_detail.quantity = cart_item.quantity
+      order_detail.save
+      end
+    cart_item.destroy_all
+    redirect_to public_orders_complete_path
+  end
+  
+  def index
+    @orders = Order.all
+  end
+  
   def confirm
   end
 
   def complete
   end
 
-  def create
-  end
-
-  def index
-  end
-
-  def show
-  end
+ 
+ 
 end
