9258dd958c091c56b07cde46d90764ccee0bca10
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-07-21 05:40:06 +0000

    “管理者ログイン機能”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admin_root_path    # ログイン後に遷移するpathを設定
-    when Public
-      root_path # ログイン後に遷移するpathを設定
-    end
-  end
 
-  def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定(publicブランチ統合後は別のリンクに変更)
-  end
+ def after_sign_in_path_for(resource_or_scope)　#複数のdeviceがある場合この記述をする
+    if resource.is_a?(Admin)
+       admin_orders_path
+    else
+       root_path
+    end
+ end
+
 end
