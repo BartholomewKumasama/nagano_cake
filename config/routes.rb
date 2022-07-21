2c67a59bd62936687deb59872d445524bbd8ca0f
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-07-21 05:48:44 +0000

    “コメント”

--- config/routes.rb
+++ config/routes.rb
@@ -30,18 +30,7 @@ Rails.application.routes.draw do
     resources :cart_items, only:[:index,:create,:update,:destroy]
     delete 'cart_items/destroy_all'
 
-<<<<<<< HEAD
-    resources :cart_items, only:[:show,:edit,:update]
-
-
-    resources :cart_items, only:[:index,:create,:update,:destroy]
-    delete 'cart_items/destroy_all'
-
-    resources :cart_items, only:[:show,:edit,:update]
-
-=======
     resources :customers, only:[:show,:edit,:update]
->>>>>>> origin/develop
     get 'customers/unsubscribe'
     patch 'customers/withdrawal'
 
