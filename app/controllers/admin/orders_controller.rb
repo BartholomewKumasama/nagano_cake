066a6b9a16b3c543ffcfbe1231b60ad0f40326b9
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-07-25 12:54:47 +0000

    “adminno”

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,23 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
-  def edit
+
+  def show
+  	@order = Order.find(params[:id])
+  # 	@order_items = @order.order_items
+  	@order_details = @order.order_details
   end
 
   def update
+  	@order = Order.find(params[:id])
+  # 	@order_items = @order.order_items
+  	@order.update(order_params)
+
+  		 redirect_to  admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+  	params.require(:order).permit(:status)
   end
 end
